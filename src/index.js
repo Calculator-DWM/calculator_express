0643761b3c52b12480e09ce031dcf0fbe21500bd
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-11-10 16:07:21 -0300

    feat: capturando os logs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const sequelize = require("./config/database");
-const Routes = require("./routes/routes")
+const bodyParser = require("body-parser");
+const morganBody = require("morgan-body");
+const Routes = require("./routes/routes");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 
 sequelize.sync().then(() => console.log("Database connected successfully"));
 
-app.use(express.json());
+app.use(bodyParser.json());
 
-app.use("/calculator", Routes)
+const log = fs.createWriteStream(
+  path.join(__dirname, "./logs", "express.log"),
+  { flags: "a" }
+);
+
+morganBody(app, {
+  noColors: true,
+  stream: log,
+});
+
+app.use("/calculator", Routes);
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
